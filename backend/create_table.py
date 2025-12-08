47940c23e5d4d8b4034893657a9a1cd3d3c9236c
AgnesTachyon-feet <kuysasa007@gmail.com>
2025-12-08 15:54:27 +0700

    start backend flask

---
+++ backend/create_table.py
@@ -0,0 +1,22 @@
+from db import get_connection
+
+def create_users_table():
+    conn = get_connection()
+    try:
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS users (
+                        user_id SERIAL PRIMARY KEY,
+                        username VARCHAR(100) UNIQUE NOT NULL,
+                        password_hash TEXT NOT NULL,
+                        email VARCHAR(100) UNIQUE NOT NULL,
+                        created_at TIMESTAMP DEFAULT NOW()
+                    )
+                               """)
+        print("สร้างตาราง users เสร็จแล้ว")
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาด: {e}")
+
+    finally:
+        conn.close()
