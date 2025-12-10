e8ec8deb3feb040529c6996da2ebb28c6d757fd5
AgnesTachyon-feet <kuysasa007@gmail.com>
2025-12-10 13:47:26 +0700

    update auth.py access token

--- backend/auth.py
+++ backend/auth.py
@@ -13,10 +13,32 @@ def login():
     password = data.get("password")
 
     if not username or not password:
-        return ("error": "username and password ไม่มีค่า"), 400
+        return {"error": "username and password ไม่มีค่า"}, 400
     
     try:
+        conn = get_connection()
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT user_id, password FROM users WHERE user_name = %s",
+                    (username)
+                )
+                user = cursor.fetchone()
         
+        if user is None:
+            return {"error": "not found user"}, 404
+        
+        user_id, user_name, hashed_passord = user
+
+        if bcrypt.checkpw(password.encode("utf-8"), hashed_passord.encode("utf-8")):
+            access_token = create_access_token(
+                identity = user_name,
+                additional_claims = {"user_id": user_id}
+            )
+
+            return jsonify({"access_token": access_token}), 200
+        else:
+            return {"erroe": "invalid password"}, 401
 
     except Exception as e:
         return {"error": str(e)}, 500
